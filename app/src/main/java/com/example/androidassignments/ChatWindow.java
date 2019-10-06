4c33d57a5a55a1fbff8e02a0bf4bf6ab25408500
Luke <lukekrete@gmail.com>
2019-10-06 16:48:58 -0400

    Finished part 3.

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -1,11 +1,8 @@
 package com.example.androidassignments;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
